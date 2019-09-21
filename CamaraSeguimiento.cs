21fe9ce07e971412a4838765fbfd2fc69fc61c44
Fpcarlosc <49990214+Fpcarlosc@users.noreply.github.com>
2019-09-21 10:52:22 +0200

    Add files

---
+++ CamaraSeguimiento.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamaraSeguimiento : MonoBehaviour
+{
+    public Transform objetivo; // Posición del objetivo al que va a seguir la cámara.
+    public float velocidadSeguimiento = 5f; // Velocidad con la que la cámara va a seguir al objetivo.
+
+    private Vector3 distancia; // Distancia de la cámara al objetivo.
+
+    // Start es llamado antes de actualizarse el primer frame
+    void Start()
+    {
+        // Distancia inicial
+        distancia = transform.position - objetivo.position;
+    }
+
+    // FixedUpdate será el método encargado de mover la cámara calculando una nueva ditancia en cada frame.
+    void FixedUpdate ()
+    {
+        // Calcula la posición de la cámara en función de la posición del objetivo más la distancia inicial.
+        Vector3 PosCamaraAObjetivo = objetivo.position + distancia;
+
+        // El método Lerp realiza movimientos suaves entre dos puntos. 
+        // En este caso entre la posición de la cámara (transform.position) y la del objetivo (PosCamaraAObjetivo) 
+        // especificando la velocidad (velocidadSeguimiento * Time.deltaTime).
+        transform.position = Vector3.Lerp (transform.position, PosCamaraAObjetivo, velocidadSeguimiento * Time.deltaTime);
+    }
+}
---
+++ MovimientoEnemigo.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MovimientoEnemigo : MonoBehaviour
+{
+    Transform jugador; // Referencia a la posición del jugador.
+    NavMeshAgent nav; // Referencia a la malla de navegación del agente.
+
+
+    void Awake ()
+    {
+        // Buscamos el GameObject con el tag "Player".
+        jugador = GameObject.FindGameObjectWithTag ("Player").transform;
+
+        // Obtenemos la referencia del componente NavMeshagent.
+        nav = GetComponent <NavMeshAgent> ();
+    }
+
+    // Usamos el método Update ya que no hacemos uso de físicas.
+    void Update ()
+    {
+        // Establecemos que el destino del agente es la posición donde está el jugador.
+        nav.SetDestination (jugador.position);
+
+    } 
+}
